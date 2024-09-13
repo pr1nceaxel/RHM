31289d5d72c0c1dd591748e94d10c8c2d35ae9bc
AndyMelone <andy.30@yahoo.com>
2024-09-13 02:02:29 +0000

    create team init

--- src/screens/employe/Employe_archive.jsx
+++ src/screens/employe/Employe_archive.jsx
@@ -1,7 +1,22 @@
+import logo from "../../assets/bloodPocketAB+.svg";
+
 const EmployeArchive = () => {
   return (
     <>
-      <p>hello</p>
+      <div className="border p-1 outline-offset-4 rounded-lg w-full h-full">
+        <div className="bg-gray-100">
+          <img src={logo} alt="logo" className="w-20 h-20 mx-auto" />
+        </div>
+        <p className="font-medium text-xs">Poche de sang</p>
+        <div className="flex flex-row justify-between items-center mt-2 mb-4 w-full">
+          <p className="bg-[#FFF8ED] py-1 px-6 rounded-2xl font-bold text-lg">
+            AB+
+          </p>
+          <p className="text-lg">2000 FCFA</p>
+        </div>
+      </div>
+
+ 
     </>
   );
 };
